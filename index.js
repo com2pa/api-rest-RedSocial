365a729ad81cdbdaf70e42eea0b5f31c59b7db52
merwil vegas <com2pa@gmail.com>
2024-11-17 10:52:37 -0400

    creando el servidor y eliminando condigo no necesario

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 // importando dependencia
-// const connection = require('./database/connection') 
 const http = require('http')
 const app= require('./app')
-
-// conexion a la base de datos
-// connection();
 
 // creando servidor de nodejs
 const server = http.createServer(app);
 server.listen(3000,()=>{
     console.log('servidor escuchando en el puerto 3000')  
 })
-
-
-// poner servidor a escuchar peticiones  http
-
