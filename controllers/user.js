a4dd8205e4da296ac7325c45a2a2b748b4f6f4f9
merwil vegas <com2pa@gmail.com>
2024-11-17 10:50:27 -0400

    la conexion no sera de manera local

--- controllers/user.js
+++ controllers/user.js
@@ -8,105 +8,105 @@ const { PAGE_URL } = require('../config');
 
 // Ruta para registrar un usuario
 
-userRouter.post('/register', async (req, res) => {
-    const{
-        Firstname,
-        Lastname,
-        nick,
-        email,
-        password,
-        }
-        = req.body
-    // verificar si lo datos existen
-    if(!Firstname ||!Lastname || !nick ||!email ||!password) return res.status(400).json({msg: 'Todos los campos son obligatorios'})
-    // verificar si el email ya existe
-    const emailExist = await User.findOne({email: req.body.email})
+// userRouter.post('/register', async (req, res) => {
+//     const{
+//         Firstname,
+//         Lastname,
+//         nick,
+//         email,
+//         password,
+//         }
+//         = req.body
+//     // verificar si lo datos existen
+//     if(!Firstname ||!Lastname || !nick ||!email ||!password) return res.status(400).json({msg: 'Todos los campos son obligatorios'})
+//     // verificar si el email ya existe
+//     const emailExist = await User.findOne({email: req.body.email})
 
-    //     // encriptar la contraseña
-        const saltRounds = 10
-        const passwordHash = await bcryt.hash(password, saltRounds)
+//     //     // encriptar la contraseña
+//         const saltRounds = 10
+//         const passwordHash = await bcryt.hash(password, saltRounds)
         
-    // crear nuevo usuario
-        const newUser = new User({
-            Firstname,
-            Lastname,
-            nick,
-            email,
-            password: passwordHash
-        })
-    // guardar usuario en la base de datos
-    const savedUser =  await newUser.save()
-    console.log('usuario guardado',savedUser)    
+//     // crear nuevo usuario
+//         const newUser = new User({
+//             Firstname,
+//             Lastname,
+//             nick,
+//             email,
+//             password: passwordHash
+//         })
+//     // guardar usuario en la base de datos
+//     const savedUser =  await newUser.save()
+//     console.log('usuario guardado',savedUser)    
 
-    //token
-    const token = jwt.sign({id: savedUser.id},  process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h'})
-    console.log('prueba de token',token)
+//     //token
+//     const token = jwt.sign({id: savedUser.id},  process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h'})
+//     console.log('prueba de token',token)
 
-    // enviar correo de confirmación
-        const transporter = nodemailer.createTransport({
-            host:'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: 'your_email@gmail.com',
-                pass: 'your_password'
-            }
-        })
+//     // enviar correo de confirmación
+//         const transporter = nodemailer.createTransport({
+//             host:'smtp.gmail.com',
+//             port: 465,
+//             secure: true,
+//             auth: {
+//                 user: 'your_email@gmail.com',
+//                 pass: 'your_password'
+//             }
+//         })
 
-        await transporter.sendMail({
-            from: process.env.EMAIL_USER, // sender address
-            to: email, // list of receivers
-            subject: "Verificacion de usuario :)  ✔", // Subject line
-            text: "'Hola'+ req.body.name + ', gracias por registrarte en nuestra red social Para confirmar tu correo, haz click en el siguiente enlace:", 
-            html: `<a href="${PAGE_URL}/verify/${savedUser.id}/${token}">Verificar Correo por favor ! </a>`, 
-          });
+//         await transporter.sendMail({
+//             from: process.env.EMAIL_USER, // sender address
+//             to: email, // list of receivers
+//             subject: "Verificacion de usuario :)  ✔", // Subject line
+//             text: "'Hola'+ req.body.name + ', gracias por registrarte en nuestra red social Para confirmar tu correo, haz click en el siguiente enlace:", 
+//             html: `<a href="${PAGE_URL}/verify/${savedUser.id}/${token}">Verificar Correo por favor ! </a>`, 
+//           });
       
-    return response.status(201).json('! usuario creado! , para continuar con el registro verificar su correo :)')
-})
+//     return response.status(201).json('! usuario creado! , para continuar con el registro verificar su correo :)')
+// })
 
-// Ruta para verificar el correo
+// // Ruta para verificar el correo
 
-userRouter.get('/verify/:id', async (req, res) => {
-    try {
-        // buscar usuario por el id
-        const token =req.params.token
+// userRouter.get('/verify/:id', async (req, res) => {
+//     try {
+//         // buscar usuario por el id
+//         const token =req.params.token
 
-        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-        const id = decodedToken.id;
+//         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+//         const id = decodedToken.id;
 
-        // cambiar el estado del usuario a verificado
-        await User.findOneAndUpdate({id,verificacion:true})
+//         // cambiar el estado del usuario a verificado
+//         await User.findOneAndUpdate({id,verificacion:true})
 
-        return res.status(200).json('! Usuario verificado correctamente!')
-    } catch (error) {
-        // encontrar el email del usuario
-        const id =request.params.id
-        const {email}=await User.findById(id)
-        // confirmar el nuevo token
-        const token = jwt.sign({id},  process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h'})
+//         return res.status(200).json('! Usuario verificado correctamente!')
+//     } catch (error) {
+//         // encontrar el email del usuario
+//         const id =request.params.id
+//         const {email}=await User.findById(id)
+//         // confirmar el nuevo token
+//         const token = jwt.sign({id},  process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h'})
         
-        // enviar correo de confirmación
-        const transporter = nodemailer.createTransport({
-            host:'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: 'your_email@gmail.com',
-                pass: 'your_password'
-            }
-        })
+//         // enviar correo de confirmación
+//         const transporter = nodemailer.createTransport({
+//             host:'smtp.gmail.com',
+//             port: 465,
+//             secure: true,
+//             auth: {
+//                 user: 'your_email@gmail.com',
+//                 pass: 'your_password'
+//             }
+//         })
 
-        await transporter.sendMail({
-            from: process.env.EMAIL_USER, // sender address
-            to: email, // list of receivers
-            subject: "Verificacion de usuario :)  ✔", // Subject line
-            text: "'Hola'+ req.body.name + ', gracias por registrarte en nuestra red social Para confirmar tu correo, haz click en el siguiente enlace:", 
-            html: `<a href="${PAGE_URL}/verify/${savedUser.id}/${token}">Verificar Correo por favor ! </a>`, 
-          });
+//         await transporter.sendMail({
+//             from: process.env.EMAIL_USER, // sender address
+//             to: email, // list of receivers
+//             subject: "Verificacion de usuario :)  ✔", // Subject line
+//             text: "'Hola'+ req.body.name + ', gracias por registrarte en nuestra red social Para confirmar tu correo, haz click en el siguiente enlace:", 
+//             html: `<a href="${PAGE_URL}/verify/${savedUser.id}/${token}">Verificar Correo por favor ! </a>`, 
+//           });
 
-          return response.status(400).json({error:'El link expiro. Se ha enviado un ¡Nuevo link! de verificacion a su correo'})
+//           return response.status(400).json({error:'El link expiro. Se ha enviado un ¡Nuevo link! de verificacion a su correo'})
           
-    }
-})
+//     }
+// })
 
 module.exports = userRouter;
--- database/connection.js
+++ database/connection.js
@@ -1,12 +1,13 @@
-const moongose = require("mongoose")
-const { PAGE_URL } = require("../config")
+const mongoose = require("mongoose")
+const { MONGO_URL } = require("../config")
+
 
 // Connect to MongoDB
 const connection =async()=>{
     try {
         // conexion a nivel local con la base de datos
-        await moongose.connect(PAGE_URL)
-        console.log("Conectando a  MongoDB mi-red_social local.....")
+        await mongoose.connect(MONGO_URL)
+        console.log("Conectando a  MongoDB mi-red_social")
     } catch (error) {
         console.error("Error connecting to MongoDB:", error)
     }
